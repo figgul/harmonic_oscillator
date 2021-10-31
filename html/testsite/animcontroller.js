87987f45d52e5bd003401744195bf3b96def16ba
Figgul <horksnacker@gmail.com>
2021-10-30 19:38:50 -0700

    slight anim controller expansion

--- html/testsite/animcontroller.js
+++ html/testsite/animcontroller.js
@@ -1,6 +1,17 @@
 //ANIMATION CONTROLLER SCRIPT
 //MNO
 
+class animStruct {
+	constructor(img_id, frames, update_freq) {
+      this.img_id = img_id;
+      this.frames = frames;
+	  this.update_freq = update_freq
+      this.current_frame = 0;
+  }
+}
+
+var frame_00X = 'assets/birdbook/page_02_panel_01_frame_00.png';
+var frame_01X = 'assets/birdbook/page_02_panel_01_frame_01.png';
 var frame_00 = 'assets/birdbook/page_02_panel_02_frame_00.png';
 var frame_01 = 'assets/birdbook/page_02_panel_02_frame_01.png';
 var frame_02 = 'assets/birdbook/page_02_panel_02_frame_02.png';
@@ -14,16 +25,23 @@ var frame_09 = 'assets/birdbook/page_02_panel_02_frame_09.png';
 var frame_10 = 'assets/birdbook/page_02_panel_02_frame_10.png';
 var frame_11 = 'assets/birdbook/page_02_panel_02_frame_11.png';
 
-var anim_img_id = document.getElementById('page_02_panel_02_anim');
+var anim_img_id_01 = document.getElementById('page_02_panel_01_anim');
+var anim_img_id_02 = document.getElementById('page_02_panel_02_anim');
 
-var frames = [frame_00, frame_01, frame_02, frame_03, frame_04, frame_05, frame_06, frame_07, frame_08, frame_09, frame_10, frame_11];
+var frames_01 = [frame_00X, frame_01X];
+var frames_02 = [frame_00, frame_01, frame_02, frame_03, frame_04, frame_05, frame_06, frame_07, frame_08, frame_09, frame_10, frame_11];
 
-var current_frame = 0;
+var freq_01 = 300;
+var freq_02 = 100;
 
-var t = setInterval(updateAnim, 100);
+var anim_1 = new animStruct(anim_img_id_01, frames_01, freq_01);
+var anim_2 = new animStruct(anim_img_id_02, frames_02, freq_02);
 
-function updateAnim() {
-  anim_img_id.src = frames[current_frame];
-  current_frame++;
-  if (current_frame >= frames.length) {current_frame = 0;}
+var t_1 = setInterval(function(){updateAnim(anim_1)}, anim_1.update_freq);
+var t_2 = setInterval(function(){updateAnim(anim_2)}, anim_2.update_freq);
+
+function updateAnim(anim) {
+  anim.img_id.src = anim.frames[anim.current_frame];
+  anim.current_frame++;
+  if (anim.current_frame >= anim.frames.length) {anim.current_frame = 0;}
 }
