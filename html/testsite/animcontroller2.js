2cde3faf7c8fd4919f17aeac6c89823079e6bf3d
Figgul <horksnacker@gmail.com>
2021-11-26 19:01:24 -0800

    html tryharding!!!!

--- html/testsite/animcontroller2.js
+++ html/testsite/animcontroller2.js
@@ -10,14 +10,39 @@ class animStruct {
   }
 }
 
-var frame_00 = 'assets/DSC02752.jpg';
-var frame_01 = 'assets/DSC02753.jpg';
+var frame_00 = 'assets/photos/DSC01128.jpg';
+var frame_01 = 'assets/photos/DSC01185.jpg';
+var frame_02 = 'assets/photos/DSC01194.jpg';
+var frame_03 = 'assets/photos/DSC01203.jpg';
+var frame_04 = 'assets/photos/DSC01204.jpg';
+var frame_05 = 'assets/photos/DSC01207.jpg';
+var frame_06 = 'assets/photos/DSC01208.jpg';
+var frame_07 = 'assets/photos/DSC01209.jpg';
+var frame_08 = 'assets/photos/DSC01210.jpg';
+var frame_09 = 'assets/photos/DSC01212.jpg';
+var frame_10 = 'assets/photos/DSC01219.jpg';
+
+/*
+var frame_00 = 'assets/photos/DSC02752.jpg';
+var frame_01 = 'assets/photos/DSC02753.jpg';
+*/
+/*
+var frame_00 = 'assets/photos/DSC09987.jpg';
+var frame_01 = 'assets/photos/DSC09988.jpg';
+*/
+/*
+var frame_00 = 'assets/photos/DSC02216.jpg';
+var frame_01 = 'assets/photos/DSC02217.jpg';
+var frame_02 = 'assets/photos/DSC02218.jpg';
+*/
 
 var anim_img_id_01 = document.getElementById('splash-flex-img-1-standard');
 
-var frames_01 = [frame_00, frame_01];
+//var frames_01 = [frame_00, frame_01];
+//var frames_01 = [frame_00, frame_01, frame_02];
+var frames_01 = [frame_00, frame_01, frame_02, frame_03, frame_04, frame_05, frame_06, frame_07, frame_08, frame_02, frame_03, frame_04, frame_05, frame_06, frame_07, frame_08, frame_09, frame_10];
 
-var freq_01 = 100;
+var freq_01 = 80;
 
 var anim_1 = new animStruct(anim_img_id_01, frames_01, freq_01);
 
