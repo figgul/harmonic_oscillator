34c2209019a05056ea3225ac987f73aec2e343f9
Figgul <horksnacker@gmail.com>
2021-11-29 21:25:09 -0800

    OFFICIAL ANIMATION STATE-MACHINE PSEUDOCODE!

--- html/testsite/animcontroller.js
+++ html/testsite/animcontroller.js
@@ -2,13 +2,24 @@
 //MNO
 
 class animStruct {
-	constructor(img_id, frames, update_freq) {
-      this.img_id = img_id;
-      this.frames = frames;
-	  this.update_freq = update_freq
-      this.current_frame = 0;
+	constructor(img_id_reference, frames_reference, update_freq_reference) {
+		this.img_id = img_id_reference;
+		this.frames = frames_reference;
+		this.update_freq = update_freq_reference;
+		this.current_frame = 0;
   }
 }
+
+class animState {
+	constructor(state_id_reference, state_update_freq_reference, reversal_boolean_reference) {
+		this.state_id = state_id_reference;
+		this.state_update_freq = state_update_freq_reference;
+		this.reversal_boolean = reversal_boolean_reference;
+	}
+}
+
+var anim_img_id_01 = document.getElementById('page_02_panel_01_anim');
+var anim_img_id_02 = document.getElementById('page_02_panel_02_anim');
 
 var frame_00X = 'assets/birdbook/page_02_panel_01_frame_00.png';
 var frame_01X = 'assets/birdbook/page_02_panel_01_frame_01.png';
@@ -24,9 +35,6 @@ var frame_08 = 'assets/birdbook/page_02_panel_02_frame_08.png';
 var frame_09 = 'assets/birdbook/page_02_panel_02_frame_09.png';
 var frame_10 = 'assets/birdbook/page_02_panel_02_frame_10.png';
 var frame_11 = 'assets/birdbook/page_02_panel_02_frame_11.png';
-
-var anim_img_id_01 = document.getElementById('page_02_panel_01_anim');
-var anim_img_id_02 = document.getElementById('page_02_panel_02_anim');
 
 var frames_01 = [frame_00X, frame_01X];
 var frames_02 = [frame_00, frame_01, frame_02, frame_03, frame_04, frame_05, frame_06, frame_07, frame_08, frame_09, frame_10, frame_11];
